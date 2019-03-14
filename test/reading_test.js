ce4c7efa5ad8d279c788c201b896e71b444ebed1
Thanaphoom Babparn <mart-tp@hotmail.com>
2019-03-15 06:30:24 +0700

    Learning skip and pagination

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,14 +2,17 @@ const assert = require("assert");
 const User = require("../src/user.js");
 
 describe("Reading users out of the database", () => {
-  let joe;
+  let joe, maria, alex, zach;
+
   beforeEach(done => {
-    joe = new User({
-      name: "Joe"
-    });
-    joe.save().then(() => {
-      done();
-    });
+    joe = new User({ name: "Joe" });
+    maria = new User({ name: "Maria" });
+    alex = new User({ name: "Alex" });
+    zach = new User({ name: "Zach" });
+
+    Promise.all([joe.save(), maria.save(), alex.save(), zach.save()]).then(() =>
+      done()
+    );
   });
 
   it("finds all users with a name of joe", done => {
@@ -27,5 +30,19 @@ describe("Reading users out of the database", () => {
       assert(user.name === "Joe");
       done();
     });
+  });
+
+  // Testing pagination
+  it("can skip and limit the result set", done => {
+    User.find({})
+      .sort({ name: 1 })
+      .skip(1)
+      .limit(2)
+      .then(users => {
+        assert(users.length === 2);
+        assert(users[0].name === "Joe");
+        assert(users[1].name === "Maria");
+        done();
+      });
   });
 });
